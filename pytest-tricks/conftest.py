4a6128cae4cff392fe2e39a704f78513892d8f34
hmzhan <hmzhan@gmail.com>
2023-01-19 22:26:10 -0500

    add pytest-tricks

--- pytest-tricks/conftest.py
+++ pytest-tricks/conftest.py
@@ -32,4 +32,46 @@ def input_file_path(tmp_path_factory, input_file_content, file_name) -> str:
     return fn
 
 
+@pytest.fixture(scope="session")
+def aws_credentials() -> None:
+    os.environ["AWS_ACCESS_KEY_ID"] = "TEST"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "TEST"
+    os.environ["AWS_SECURITY_TOKEN"] = "TEST"
+    os.environ["AWS_SESSION_TOKEN"] = "TEST"
 
+
+@pytest.fixture(scope="session")
+def moto_s3_session(aws_credentials, s3_bucket, file_name, input_file_path):
+    with mock_s3():
+        session = Session()
+        s3 = session.client("s3", region_name="us-east-1")
+        s3.create_bucket(Bucket=s3_bucket)
+        s3.upload_file(input_file_path, s3_bucket, file_name)
+        buckets = s3.list_buckets()["Buckets"]
+        assert len(buckets) == 1
+        assert buckets[0]["Name"] == s3_bucket
+        yield session
+
+
+@pytest.fixture(scope="session")
+def mocked_s3_client(moto_s3_session):
+    yield moto_s3_session.client("s3")
+
+
+@pytest.fixture(scope="session")
+def example_data():
+    input_data = pd.DataFrame({
+        "Name": ["John Smith", "James Harold", "Aura Anderson"],
+        "DOB": ["19900101" "19800101", "20000117"],
+        "Location": ["Boston", "New York", "Amherst"]
+    })
+    return input_data
+
+
+@pytest.fixture(scope="session")
+def example_data2():
+    input_data = pd.DataFrame({
+        "Name": ["John Smith", "James Harold", "Aura Anderson"],
+        "Score": [10, 20, 30]
+    })
+    return input_data
